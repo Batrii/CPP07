160caa165ec05b0aa969f982c686a591dce964d7
Batrii <dhj.nafiai@gmail.com>
2025-12-05 17:39:16 +0100

    add tests to ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -11,5 +11,34 @@ int main(){
     {
         std::cout << e.what() << std::endl;
     }
+    //--------------------------------------------------
+    try{
+        Array<std::string> strArr(3);
+        strArr[0] = "Hello";
+        strArr[1] = "World";
+        strArr[2] = "!";
+        unsigned int i = 0;
+        while (i < strArr.size())
+        {
+            std::cout << strArr[i] << " ";
+            i++;
+        }
+        std::cout << std::endl;
+    }catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    //--------------------------------------------------
+
+    try{
+        Array<double> emptyArr;
+        std::cout << "Size of emptyArr: " << emptyArr.size() << std::endl;
+        // Attempting to access an element in the empty array
+        std::cout << emptyArr[0] << std::endl;
+    }catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
     return 0;
 }
